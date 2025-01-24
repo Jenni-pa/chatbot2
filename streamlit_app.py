e57c04b1fc0683ddd5eedd698b3b342f72b2fb3e
Jennifer <jennifer.park@tum.de>
2025-01-24 20:14:48 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,19 @@ def run_command_page():
 
     uploaded_file = st.file_uploader("Choose a file",type=['txt'])
     NameOfCompany = st.text_input('Company name:')
+    st.session_state.newCompany = None
+
     if st.button("add company"):
-        newCompanyId = fileupload(uploaded_file.read()).id
+        st.session_state.newCompany = fileupload(uploaded_file.read())
         st.write("Company added successfully")        
 
 
-    if st.button("Send request now"):
-        result = askgpt("Explain BPs stance to sustainability and compare with "+NameOfCompany, newCompanyId)
+    if st.button("Send request now", "without_attachment") and st.session_state.newCompany is None:
+        result = askgpt(f"Explain BPs stance to sustainability and compare with {NameOfCompany}" , None)
+        st.write(f"Command Output: {result}")
+
+    if st.button("Send request now", "with_attachment") and st.session_state.newCompany is not None:
+        result = askgpt(f"Explain BPs stance to sustainability and compare with {NameOfCompany}" , st.session_state.newCompany.id)
         st.write(f"Command Output: {result}")
 
 run_command_page()
