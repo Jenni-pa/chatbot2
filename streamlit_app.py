cb9e7ba02988692863fb87e5c384bfcd548b1d00
Jennifer <jennifer.park@tum.de>
2025-01-25 15:45:24 +0100

    try to actually comment out da other stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,29 +183,29 @@ elif len(pickedCompanies) == 3:
     else:
         st.markdown(prompts["3 companies"].format(category=chosenCategory, companyA=pickedCompanies[0], companyB=pickedCompanies[1], companyC=pickedCompanies[2]))
 
-''' import streamlit as st
-from cod import askgpt, fileupload
+# import streamlit as st
+# from cod import askgpt, fileupload
 
-def run_command_page():
-    st.title("Ask GPT")
-    st.write("Testing calling the script to interact with the openai api")
-
-    uploaded_file = st.file_uploader("Choose a file",type=['txt'])
-    NameOfCompany = st.text_input('Company name:')
-    st.session_state.newCompany = None
-
-    if st.button("add company"):
-        st.session_state.newCompany = fileupload(uploaded_file.read())
-        st.write("Company added successfully")        
-
-
-    if st.button("Send request now", "without_attach,emt") and st.session_state.newCompany is None:
-        result = askgpt(f"Explain BPs stance to sustainability and compare with {NameOfCompany}" , None)
-        st.write(f"Command Output: {result}")
-
-    if st.button("Send request now", "with_attachment") and st.session_state.newCompany is not None:
-        result = askgpt(f"Explain BPs stance to sustainability and compare with {NameOfCompany}" , st.session_state.newCompany.id)
-        st.write(f"Command Output: {result}")
-
-run_command_page()
-'''
+#def run_command_page():
+#    st.title("Ask GPT")
+#    st.write("Testing calling the script to interact with the openai api")
+#
+#    uploaded_file = st.file_uploader("Choose a file",type=['txt'])
+#    NameOfCompany = st.text_input('Company name:')
+#    st.session_state.newCompany = None
+#
+#    if st.button("add company"):
+#        st.session_state.newCompany = fileupload(uploaded_file.read())
+#        st.write("Company added successfully")        
+#
+#
+#    if st.button("Send request now", "without_attach,emt") and st.session_state.newCompany is None:
+#        result = askgpt(f"Explain BPs stance to sustainability and compare with {NameOfCompany}" , None)
+#        st.write(f"Command Output: {result}")
+#
+#    if st.button("Send request now", "with_attachment") and st.session_state.newCompany is not None:
+#        result = askgpt(f"Explain BPs stance to sustainability and compare with {NameOfCompany}" , st.session_state.newCompany.id)
+#        st.write(f"Command Output: {result}")
+#
+#run_command_page()
+#
