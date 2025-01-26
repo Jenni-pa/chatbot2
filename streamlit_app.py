f38a98e2845fc96b7dfa73fa862e305e69527c92
Jennifer <jennifer.park@tum.de>
2025-01-26 16:39:55 +0100

    same try #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -203,43 +203,44 @@ if st.button("send question"):
         else:
             result = askgpt(prompts["3 companies"].format(category=chosenCategory, companyA=pickedCompanies[0], companyB=pickedCompanies[1], companyC=pickedCompanies[2]), st.session_state.newCompany.id if st.session_state.newCompany is not None else None)
 
+barchart = None
+if result != None:
+    response = re.sub(r"\【.*\】", "", result)
+    if (graphicalOutput == "yes" and chosenCategory == "CO2 emissions"):
+        try: 
+            # bar chart maker for c02 2 companies
+            start_marker = "---chart-data-start---"
+            end_marker = "---chart-data-end---"
+
+            data_start = result.find(start_marker) + len(start_marker)
+            data_end = result.find(end_marker)
+            data = result[data_start:data_end].strip()
+
+            # Convert to DataFrame
+            df = pd.read_csv(StringIO(data))
+
+            # make barchart
+            barchart = st.bar_chart(df.set_index('Year'))
+
+            # Use regex to remove last paragraph unless its the conclusion/summary
+            # response = re.sub(r"(\#{3}(?!.*(summary|conclusion))(?!.*\#{3})).*", "", response, flags = re.DOTALL | re.IGNORECASE)
+            # use regex to remove chart data in case upper regex did not
+            response = re.sub(r"(---chart-data-start---).*(---chart-data-end---)", "", response, flags = re.DOTALL)
+            # add barchart to response
+            response += barchart
+        except:
+            response = re.sub(r"(\#{3}(?!.*(summary|conclusion))(?!.*\#{3})).*", "", response, flags = re.DOTALL | re.IGNORECASE)
+            response += "<br/>**Graphic could not be generated**"
+else:
+    response = ""
+     
+
 if 'listOfResponses' not in st.session_state :
     st.session_state['listOfResponses'] = []
 
-if result != None:
-    response = re.sub(r"\【.*\】", "", result)
-    if response is not "":
-        st.session_state['listOfResponses'].append(response)
-else:
-    response = ""
+if response is not "":
+    st.session_state['listOfResponses'].append(response)
 
 for message in st.session_state['listOfResponses']:
     with st.chat_message("user", avatar="Avatar.png"):
         st.markdown(message, unsafe_allow_html=True)
-
-
-if (graphicalOutput == "yes" and chosenCategory == "CO2 emissions"):
-    try: 
-        # bar chart maker for c02 2 companies
-        start_marker = "---chart-data-start---"
-        end_marker = "---chart-data-end---"
-
-        data_start = result.find(start_marker) + len(start_marker)
-        data_end = result.find(end_marker)
-        data = result[data_start:data_end].strip()
-
-        # Convert to DataFrame
-        df = pd.read_csv(StringIO(data))
-
-        # Display in Streamlit
-        st.bar_chart(df.set_index('Year'))
-
-        # Use regex to remove last paragraph unless its the conclusion/summary
-        response = re.sub(r"(\#{3}(?!.*(summary|conclusion))(?!.*\#{3})).*", "", response, flags = re.DOTALL | re.IGNORECASE)
-        # use regex to remove chart data in case upper regex did not
-        response = re.sub(r"(---chart-data-start---).*(---chart-data-end---)", "", response, flags = re.DOTALL)
-    except:
-        # response = re.sub(r"(\#{3}(?!.*(summary|conclusion))(?!.*\#{3})).*", "", response, flags = re.DOTALL | re.IGNORECASE)
-        response += "<br/>**Graphic could not be generated**"
-     
-
